7b3497e218232aaeac7b4ec88c55f0a43f23e473
harinimaniam <harinimaniam2001@gmail.com>
2020-09-15 12:08:47 +0530

    Logo for Krscode

--- src/components/ToolBar.js
+++ src/components/ToolBar.js
@@ -3,12 +3,12 @@ import { inject, observer } from 'mobx-react';
 import AccountMenu from './AccountMenu';
 import { Dropdown, Menu, Layout, Typography, Row, Col } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
-import ownerLogo from '../images/pm-power.png';
+import ownerLogo from '../images/krscode.png';
 
 const { Text } = Typography;
 const { Header } = Layout;
 
-const caption = "PM Power Consulting";
+const caption = "KRS Code";
 
 @inject("appStore")
 @observer
@@ -22,16 +22,16 @@ export default class ToolBar extends Component {
     renderRoleTabs = () => {
         const appStore = this.props.appStore;
         if (!appStore.isLoggedIn()) {
-            return ( 
-                <Col span={17} style={{textAlign:"right"}}>
+            return (
+                <Col span={20} style={{ textAlign: "right" }}>
                     <Text strong={true}>Ferris - The Coaching Assistant</Text>
                 </Col>
-            )    
+            )
         }
 
         return (
-            <Col span={12}>
-                <Menu mode="horizontal" defaultSelectedKeys={['0']} style={{borderStyle:"none"}}>
+            <Col span={14}>
+                <Menu mode="horizontal" defaultSelectedKeys={['0']} style={{ borderStyle: "none" }}>
                     {
                         appStore.menus.map((item, index) => (
                             <Menu.Item key={index} onClick={this.handleEvent} >{item.label}</Menu.Item>)
@@ -44,27 +44,23 @@ export default class ToolBar extends Component {
 
     renderRightMenu() {
         const appStore = this.props.appStore;
-        if (!appStore.isLoggedIn()) {
+        if (appStore.isLoggedIn()) {
             return (
-                <></>
+                <Col span={6} style={{ textAlign: "right" }}>
+                    <Dropdown.Button overlay={<AccountMenu />} trigger={['click']} icon={<UserOutlined />}>
+                        {appStore.credentials.username}
+                    </Dropdown.Button>
+                </Col>
             )
         }
-
-        return (
-            <Col span={6}>
-                <Dropdown.Button overlay={<AccountMenu />} trigger={['click']} icon={<UserOutlined />}>
-                    {appStore.credentials.username}
-                </Dropdown.Button>
-            </Col>
-        )
     }
 
     render() {
         return (
             <Header style={{ position: 'fixed', zIndex: 1, width: '100%', background: '#fff', padding: "0px 5px 0px 5px" }}>
                 <Row>
-                    <Col span={6}>
-                        <img src={ownerLogo} align="left" width="100" height="62" title={caption} />
+                    <Col style={{ paddingTop: 5 }} span={4}>
+                        <img src={ownerLogo} align="left" width="26" height="52" title={caption} />
                     </Col>
                     {this.renderRoleTabs()}
                     {this.renderRightMenu()}
